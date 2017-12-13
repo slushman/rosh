12506e3911a70c24bdacd0b028a5b153a93d700a
Chris Wilcoxson <chris@slushman.com>
2017-12-13 16:31:31 -0600

    Replaces previous code for comments navigation with core function the_comments_navigation() to match _s #884.

--- comments.php
+++ comments.php
@@ -55,17 +55,7 @@ do_action( 'rosh_comments_before' );
 
 		?></h2><!-- .comments-title --><?php
 
-		if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through?
-
-			?><nav id="comment-nav-above" class="navigation comment-navigation">
-				<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'rosh' ); ?></h2>
-				<div class="nav-links">
-					<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'rosh' ) ); ?></div>
-					<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'rosh' ) ); ?></div>
-				</div><!-- .nav-links -->
-			</nav><!-- #comment-nav-above --><?php
-
-		endif; // Check for comment navigation.
+		the_comments_navigation();
 
 		?><ol class="comment-list"><?php
 
@@ -76,17 +66,7 @@ do_action( 'rosh_comments_before' );
 
 		?></ol><!-- .comment-list --><?php
 
-		if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through?
-
-			?><nav id="comment-nav-below" class="navigation comment-navigation">
-				<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'rosh' ); ?></h2>
-				<div class="nav-links">
-					<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'rosh' ) ); ?></div>
-					<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'rosh' ) ); ?></div>
-				</div><!-- .nav-links -->
-			</nav><!-- #comment-nav-below --><?php
-
-		endif; // Check for comment navigation.
+		the_comments_navigation();
 
 	endif; // Check for have_comments().
 
